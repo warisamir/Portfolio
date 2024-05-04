f2829331b4c8a8b7ccc466e14066927bc1e73162
WarisAmir <warisamir1918@yahoo.com>
2024-05-04 09:51:56 +0530

    Update Resume.js

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography, Link } from '@material-ui/core';
 import { TextDecrypt } from '../content/TextDecrypt';
-import ResumePDF from  '../../assets/WarisAmirResume1.pdf'
+import ResumePDF from  '../../assets/WarisAmir_resume.pdf'
 import {
   ResumeIcon
 } from '../content/ResumeButton';
